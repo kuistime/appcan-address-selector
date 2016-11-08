e48f4adea900659e391316a21e79ae0fd1972f2d
mitty.q <mitty.q@outlook.com>
2016-11-08 17:36:26 +0800

    init

---
+++ source/json2tree.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var _ = require('underscore');
+
+var path_from ='../data/plain.json';
+var path_to ='../data/tree.json';
+
+var ds=fs.readFileSync(path_from).toString();
+var da=JSON.parse(ds);
+
+var prov = _.filter(da, function(item){
+    var s=item.code;
+    return s.substr(2,4)=='0000';
+});
+
+var city = _.filter(da, function(item){
+    var s=item.code;
+    return s.substr(2,2)!='00' && s.substr(4,2)=='00';
+});
+
+var zone = _.filter(da, function(item){
+    var s=item.code;
+    return s.substr(4,2)!='00';
+});
+
+var zone_g = _.groupBy(zone, function(item){
+    return item.code.substr(0,4);
+});
+
+// zone_g_p
+_.each(zone_g, function(o, key, list){
+    list[key] = _.reduce(o, function(memo, oo){
+        memo.push(oo.name);
+        return memo;
+    }, []);
+});
+
+_.each(city, function(item,key,list){
+    var _key = item.code.substr(0,4);
+    item.sub = zone_g[_key]||[];
+});
+
+var city_g=_.groupBy(city,function(item){return item.code.substr(0,2)});
+
+_.each(prov, function(item,index,list){
+    var _key = item.code.substr(0,2);
+    item.sub = city_g[_key];
+});
+
+var res = prov;
+
+var fd = fs.openSync(path_to, 'a');
+var data = JSON.stringify(res);
+fs.writeSync(fd, data);
---
+++ source/txt2json.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+var path_from = '../data/data.txt';
+var path_to = '../data/plain.json';
+
+var stat = fs.statSync(path_from);
+
+var buffer = fs.readFileSync(path_from);
+var s = buffer.toString();
+var arr = s.split(/\n/);
+//console.log(arr.length);
+
+var res = arr.map(function(o,i,a){
+  var tmp = o.split(/\s+/);
+  return {
+    code: tmp[0],
+    name: tmp[1]
+  }
+});
+//console.log(res);
+
+var errCount = 0;
+res.forEach(function(o){
+  if(o.code&&o.name){
+    //
+  }else{
+    errCount++;
+    console.log(o);
+  }
+});
+//console.log("errCount: %s", errCount);
+
+var fd = fs.openSync(path_to, 'a');
+var data = JSON.stringify(res);
+fs.writeSync(fd, data);
